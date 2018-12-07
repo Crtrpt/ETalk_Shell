cdc1cfde0d13af8abc548dba15face9e3d8b54d6
jingzhixiang <jingzhixiang@chupinxiu.com>
2018-12-07 17:50:57 +0800

    '修改帮助文档'

--- ETalk_Shell/Program.cs
+++ ETalk_Shell/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ETalk_Shell
 {
@@ -6,7 +7,53 @@ namespace ETalk_Shell
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ParseArgs(args);
+        }
+
+        static void ParseArgs(string[] args)
+        {
+            try
+            {
+                if (args[0] == "-h" || args[0] == "--help" || args[0] == "h")
+                {
+                    Help();
+                }
+
+                switch (args[0])
+                {
+                        case "login":
+                            Console.WriteLine("TODO");
+                            break;
+                        default:
+                            Help();
+                            break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Help();
+            }
+        }
+
+        static void Help()
+        {
+            Console.Write(@"
+Usage: et [COMMAND]... [OPTION]...
+
+ login 
+    -u     --username     login username
+    -p     --password     login password
+ group 
+    -l  --List          get group list
+
+ exit                 logout Et network
+    -v --Version  output version information and exit
+
+Examples:
+et login 
+            ");
+            Environment.Exit(0);
         }
     }
 }
